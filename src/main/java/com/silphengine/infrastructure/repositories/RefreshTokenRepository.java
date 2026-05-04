91a9b06ccd9cd436576c22eac190f977d506a432
cadimodev <cadimodev@gmail.com>
2026-05-04 11:17:31 +0200

    refactor: remove unnecessary method in RefreshTokenRepository

--- src/main/java/com/silphengine/infrastructure/repositories/RefreshTokenRepository.java
+++ src/main/java/com/silphengine/infrastructure/repositories/RefreshTokenRepository.java
@@ -11,6 +11,4 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     Optional<RefreshToken> findByToken(String token);
 
     Optional<RefreshToken> findByUser(User user);
-
-    void deleteByToken(String token);
 }
